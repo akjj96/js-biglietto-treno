a262cd6fa9607e16b3e4c941fdd3970ae612d0bc
akjj96 <akimjj96@gmail.com>
2022-03-24 14:29:14 +0100

    index:lavoro

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let userName = prompt('Numero di km che vuoi percorrere?')
+console.log(userName);
+
+let userAge = prompt("Età del passegero");
+console.log(userAge);
+
+let prezzoAlKilometro = 0,21;
+let prezzoBigliettoSenzaSconto = prezzoAlKilometro * percorrenza;
+console.log(prezzoBigliettoSenzaSconto);
+
+if (eta == "minorenne") {
+ ((prezzoBigliettoSenzaSconto / 100) * 80);
+   offerta = "Sconto Minorenne";
+} else if (eta == "over65") {
+ ((prezzoBigliettoSenzaSconto / 100) * 60);
+   offerta = "Sconto Maggiorenne";
+} else {
+   (prezzoBigliettoSenzaSconto);
+}
+
+
+
+document.getElementById('treno').innerHTML 
